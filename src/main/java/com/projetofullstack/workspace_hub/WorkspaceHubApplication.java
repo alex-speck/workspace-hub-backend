3a52b35b327de7348a37b85ce6162d70b42d4f16
ALEX BARANOSKI SPECK <alexbaranoski08@gmail.com>
2026-03-19 19:39:48 -0300

    primeiro commit

---
+++ src/main/java/com/projetofullstack/workspace_hub/WorkspaceHubApplication.java
@@ -0,0 +1,13 @@
+package com.projetofullstack.workspace_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkspaceHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkspaceHubApplication.class, args);
+	}
+
+}
