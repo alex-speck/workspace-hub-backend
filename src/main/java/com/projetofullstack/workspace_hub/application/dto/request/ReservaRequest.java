ac868517ddf40347cbe4ddfb66a684960e74c892
alex-speck <alexbaranoski08@gmail.com>
2026-06-04 00:45:04 -0300

    minor fix

--- src/main/java/com/projetofullstack/workspace_hub/application/dto/request/ReservaRequest.java
+++ src/main/java/com/projetofullstack/workspace_hub/application/dto/request/ReservaRequest.java
@@ -1,6 +1,7 @@
 package com.projetofullstack.workspace_hub.application.dto.request;
 
 import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
@@ -9,7 +10,7 @@ import java.time.LocalTime;
 public record ReservaRequest(
         Long clienteId,
         Long espacoId,
-        @Future(message = "A data deve ser no futuro")
+        @FutureOrPresent(message = "A data deve ser no futuro")
         @NotNull(message = "A data não pode ser nula")
         LocalDate data,
         @NotNull(message = "Os horarios devem ser informados")
