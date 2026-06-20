f1c0f3e47fc989713eac5a2d4e0d9c513d333515
alex-speck <alexbaranoski08@gmail.com>
2026-06-19 22:29:18 -0300

    deploy com sucesso

--- src/main/java/com/projetofullstack/workspace_hub/WorkspaceHubApplication.java
+++ src/main/java/com/projetofullstack/workspace_hub/WorkspaceHubApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WorkspaceHubApplication {
 
-	// public static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(WorkspaceHubApplication.class, args);
 	}
 
